b3b99d49ce66061fdc76c64b5f78799019653d20
louis <louisbralion@gmail.com>
2024-06-12 17:05:48 +0200

    Appli java Reveillo'train

---
+++ src/main/java/be/bralion/appreveillotrain/Launcher.java
@@ -0,0 +1,9 @@
+package be.bralion.appreveillotrain;
+
+import be.bralion.appreveillotrain.controller.MainController;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainController.main(args);
+    }
+}
